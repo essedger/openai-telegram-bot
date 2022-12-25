6a0d40c30c2cf14a11d3b4e82d948c49cf1b7fb2
Essedger <essedger@gmail.com>
2022-12-25 11:24:02 +0400

    disable reaction on start bot command

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ bot = telebot.TeleBot(TELEGRAM_BOT_API_KEY)
 
 @bot.message_handler(func=lambda _: True)
 def handle_message(message):
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=message.text,
-        temperature=0.0,
-        max_tokens=1024,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-    )
-    bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
+    if message.text != '/start':
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=message.text,
+            temperature=0.0,
+            max_tokens=1024,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+        )
+        bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
 
 
 bot.polling()
