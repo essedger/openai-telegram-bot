abbc3110641dcf76bbae823e40100d63294727f2
Essedger <essedger@gmail.com>
2022-12-25 11:16:29 +0400

    openai params

--- main.py
+++ main.py
@@ -12,12 +12,11 @@ def handle_message(message):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=message.text,
-        temperature=0.5,
-        max_tokens=60,
+        temperature=0.0,
+        max_tokens=1024,
         top_p=1.0,
-        frequency_penalty=0.5,
+        frequency_penalty=0.0,
         presence_penalty=0.0,
-        stop=["You:"]
     )
     bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
 
